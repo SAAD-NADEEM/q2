7706287a15e5bfd2008060e56dc00c858bf6a226
SAAD-NADEEM <saadngaming99090@gmail.com>
2024-10-19 13:53:32 -0700

    fix typescript errors

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Navbar from "../components/Header/Navbar"
 
 function products() {
 
--- src/app/products/product1/page.tsx
+++ src/app/products/product1/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link"
 
 function Product1() {
 
     return(
         <div>
             <h1>Product 1</h1>
-            {/* <Link href='/'>Back</Link> */}
         </div>
     )
 }
